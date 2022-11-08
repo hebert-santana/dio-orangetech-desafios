ed40f0b82355dc51c16269a5eb5b0816ee9bb015
Hebert Santana <102166830+hebert-santana@users.noreply.github.com>
2022-11-08 17:41:27 -0300

    add button

--- dio/src/App.js
+++ dio/src/App.js
@@ -1,8 +1,13 @@
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
-      Olá React
+      <h1>Olá React</h1>
+      <Button title="Entrar" />
+      <Button title="Fechar" />
+      <Button title="Abrir" />
+      <Button title="Salvar" />
     </div>
   );
 }
